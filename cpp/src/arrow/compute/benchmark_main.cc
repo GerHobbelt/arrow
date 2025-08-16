8d58494a5d3fb93511a61760355df506490ccac0
Ger Hobbelt <ger@hobbelt.com>
2025-08-16 21:57:54 +0200

    WS

--- cpp/src/arrow/compute/benchmark_main.cc
+++ cpp/src/arrow/compute/benchmark_main.cc
@@ -26,7 +26,8 @@ int main(int argc, char** argv) {
 
   // Initialize and run benchmarks
   ::benchmark::Initialize(&argc, argv);
-  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv))
+    return 1;
   ::benchmark::RunSpecifiedBenchmarks();
   ::benchmark::Shutdown();
   return 0;
